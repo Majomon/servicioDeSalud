c71f161aafcf52742096ece4e74432be457fc5d5
DaniTadd <danielgarciataddia@gmail.com>
2023-05-24 23:09:26 -0300

    Se corrige funcionalidad del formulario

--- js/Contacto.js
+++ js/Contacto.js
@@ -35,19 +35,25 @@ function validarConsulta() {
         document.getElementById("consulta-error").innerHTML = "";
     }
 }
-/* abrir una ventana de alerta con los datos ingresados */
-const formulario = document.getElementById('Formulario');
 
-formulario.addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita que el formulario se envíe
+/* FUNCION PARA RECIBIR LOS DATOS DE CONSULTA DEL PACIENTE QUE SE PONE EN CONTACTO */
 
-  const opciones = document.getElementById('opciones').value;
-  const nombre = document.getElementById('nombre').value;
-  const telefono = document.getElementById('telefono').value;
-  const email = document.getElementById('email').value;
-  const consulta = document.getElementById('consulta').value;
+const form = document.querySelector('#Formulario')
+        form.addEventListener('submit', handleSubmit)
 
-  const mensajeAdvertencia = ` Opcion elegida: ${opciones}\nNombre: ${nombre}\nEmail: ${email}\nTelefono: ${telefono}\nconsulta: ${consulta}`;
+        async function handleSubmit(event) {
+            event.preventDefault();
+            const form = new FormData(this);
+            const response = await fetch(this.action, {
+                method: this.method,
+                body: form,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+            if (response.ok) {
+                this.reset(),
+                alert('Gracias por contactarnos. Te escribiremos a la brevedad')
+            }
 
-  window.alert(mensajeAdvertencia);
-});
+        }
