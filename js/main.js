16718d6f933dc82beba62047c772ecefb9b396bc
Majomon <mauri.monzon91@gmail.com>
2023-05-24 23:36:40 -0300

    mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("Hola");
+const carouselImages = document.querySelectorAll('.carousel .imgInicial');
+let currentImageIndex = 0;
+
+function showNextImage() {
+  carouselImages[currentImageIndex].classList.remove('active');
+  currentImageIndex = (currentImageIndex + 1) % carouselImages.length;
+  carouselImages[currentImageIndex].classList.add('active');
+}
+
+// Mostrar la primera imagen inicialmente
+carouselImages[currentImageIndex].classList.add('active');
+
+// Cambiar de imagen cada 2 segundos
+setInterval(showNextImage, 2000);
